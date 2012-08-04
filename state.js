3d1b1e809c21d8b344d788c6794a1cfeb131faf0
Keebuhm Park <kpark@kpark-mn.linkedin.biz>
2012-08-03 21:34:52 -0700

    added movement to state.js

--- state.js
+++ state.js
@@ -5,6 +5,10 @@ var state,
   moveQueue;
 
 var numCharacters = 1;
+var max_ground_x = 50;
+var max_ground_y = 50;
+var gravity = -5;
+var max_air_jumps = 1;
 
 // Private Helpers
 // ===============
@@ -12,6 +16,12 @@ var initCharacter = function (characterId) {
   return {
     x: characterId * 50,
     y: 10,
+    v_x: 0,
+    v_y: 0,
+    a_x: 0,
+    a_y: 0,
+    onGround: true,
+    airJumps: 0,
     damage: 0
   };
 };
@@ -19,12 +29,66 @@ var initCharacter = function (characterId) {
 var runMove = function (characterId) {
   var character = state.characters[characterId];
   switch (moveQueue[characterId]) {
+    case 'none':
+      character.a_x = 0;
+      character.a_y = gravity;
+      break;
+    // Basic movement
     case 'left':
-      character.x -= 1;
+      if (character.onGround) {
+        character.a_x = -5;
+      } else {
+        character.a_x = -3;
+      }
       break;
     case 'right':
-      character.x += 1;
+      if (character.onGround) {
+        character.a_x = 5;
+      } else {
+        character.a_x = 3;
+      }
       break;
+    case 'up':
+      if (character.onGround) {
+        character.a_y = 5;
+      } else {
+        if character.airJumps<=max_air_jumps{
+          character.a_y = 3;
+          character.airJumps++;
+        }
+      }
+      break;
+    case 'down':
+      if (character.onGround) {
+        character.a_y = -5;
+      } else {
+        character.a_y = -3;
+      }
+      break;
+    // Basic attacks
+    case 'a':
+    case 'up_a':
+    case 'down_a':
+    case 'left_a':
+    case 'right_a':
+    // Special moves
+  }
+
+  var updateCharacterMotion = function (characterId) {
+    var character = state.characters[characterId];
+    var dt = 1/24;
+    // Velocity calculation
+    //Characters have a max ground velocity, no max air velocity
+    if (character.onGround) {
+      character.v_x = min(max_ground_x, character.v_x + character.a_x * dt);
+      character.v_y = max(0, min(max_ground_y, character.v_y + character.a_y * dt));
+    } else {
+      character.v_x += character.a_x * dt;
+      character.v_y += character.a_y * dt;
+    }
+    // Position calculation
+    character.x += character.v_x * dt;
+    character.y += character.v_y * dt;
   }
 };
 
@@ -42,11 +106,19 @@ module.exports = {
     }
     moveQueue = [];
   },
+
   runFrame: function () {
-    var i;
-    for (i = 0; i < numCharacters; i++) {
-      runMove(i);
+    // Calculate position using velocity / acceleration
+    for (var i = numCharacters; i++) {
+      updateCharacterMotion(i);
     }
+    // Attacks
+    for (var i = numCharacters; i++) {
+      //attacks(i);
+      //detectHits(i);
+    }
+    // Detect collision (optional)
+    // Detect ground for each char
   },
   setMove: function (player, move) {
     moveQueue[player] = move;
